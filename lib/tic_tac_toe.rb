e322bdce02846e5fee8f5713d1eefba423b60f79
Lorena <lorena.giddens@outlook.com>
2018-04-25 22:10:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,6 +17,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+end
 
 def position_taken?(board, index)
   if board[index] == " " || 
